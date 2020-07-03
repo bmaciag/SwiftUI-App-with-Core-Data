ca6a4af25f7c8de778339494cf25b8cc9d2c2d64
bmaciag <barm@live.de>
2020-07-03 13:06:04 +0200

    Initial Commit

---
+++ SwiftUIAppWithCoreData/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIAppWithCoreData
+//
+//  Created by Bartholomäus Maciag on 03.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAppWithCoreData/SwiftUIAppWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppWithCoreDataApp.swift
+//  SwiftUIAppWithCoreData
+//
+//  Created by Bartholomäus Maciag on 03.07.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
